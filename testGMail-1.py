8900282c21832d778d1e898b2f8a861931ebb831
Dwayne Linde <lindedwayne@gmail.com>
2019-10-20 23:27:53 -0700

    The Gmail login test logic works. Now, to make it more efficient in the next version.

--- testGMail-1.py
+++ testGMail-1.py
@@ -7,62 +7,74 @@ from selenium.webdriver.common.keys import Keys
 
 # Change to YouTube Test Account
 user = "microdwaynedev@gmail.com"
-pwd = "Faceraigi1!"
+pwd = "Micrraigi1!"
+
 
 # driver = webdriver.Firefox()
 driver = webdriver.Firefox(executable_path='/Library/Frameworks/Python.framework/Versions/3.7/bin/geckodriver')
 
-driver.get("https://www.youtube.com")
+# Putting this in to prevent race conditions, and to allow all of the elements
+# on the page to be found.
+
+driver.implicitly_wait(20) #seconds.
+driver.get("https://www.gmail.com")
 
 # Setting the size so the window is smaller.
 # Did this so that I could adequately see the interpreter
 # error messages behind the window. 
 
-# driver.set_window_size(1088, 788)
+driver.set_window_size(1088, 788)
 
-sleep(20)
+sleep(10)
 
 # Now, click on the 'Sign In' button on the top right.
 
-elem = driver.find_element_by_id("text")
+#elem = driver.find_element_by_class_name("h-c-header__nav-li-link ")
 
-sleep(20)
+# elem.click()
 
-elem.click()
 
-sleep(20)
+# This makes sure that I am on the right page.
+assert "Gmail" in driver.title
 
-assert "YouTube" in driver.title
-
-elem = driver.find_element_by_id(pwd)
-
-sleep(20)
+elem = driver.find_element_by_id("identifierId")
 
 elem.send_keys(user)
-sleep(5)
 
 elem.send_keys(Keys.RETURN)
 
 # Get sleep working. This works!
-sleep(20)
-# event.wait does not work.
-# event(elem).wait(5)
 
 # This crashes; poss. solution here:
 # https://stackoverflow.com/questions/49864965/org-openqa-selenium-elementnotinteractableexception-element-is-not-reachable-by
 
+# This is the class for the element of the box.
+# elem = driver.find_element_by_css_selector("HHWSud")
 
-elem = driver.find_element_by_id("password")
+# PASSWORD FIELD
+# XPATH relpath: //input[@name='password']
 
-elem.send_keys("Faceraigi1!")
-# event().wait(1)
+sleep(20)
+
+elem = driver.find_element_by_xpath("//input[@name='password']")
+
+sleep(20)
+
+
+# This is the class for the element of the text field in the box.
+
+elem.send_keys(pwd)
+
+sleep(20)
+
 
 # Get sleep working.
-sleep(20)
 
 elem.send_keys(Keys.RETURN)
 
-# Get sleep working.
 sleep(20)
+
+
+# Get sleep working.
 
 driver.close()
